0a8b29676fbfc14cac3509dffe9c5799a8e546a7
rscerb1 <rscerb1@students.towson.edu>
2021-11-30 17:32:25 -0500

    updates

--- gui/fillDB.sql
+++ gui/fillDB.sql
@@ -32,39 +32,40 @@ VALUES
     (0,0,773512,'Amphibian','Wyoming Toad');
 
 
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2020-09-13',17245,'Heinicke',167680, 'Juvenile');
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-05-01',29563,'Yellow Beach',161980, 'Juvenile');
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-05-01',2534,'Yellow Beach',773512, 'Juvenile');
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-05-01',28433,'Eagle Peak',161980, 'Egg');
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-05-11',47664,'Eagle Peak',161980, 'Adult');
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-10-23',53456,'Bear River',161980, 'Adult');
+-- INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2020-09-13',17245,'Heinicke',167680);
+-- INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-05-01',29563,'Yellow Beach',161980);
+-- INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-05-01',2534,'Yellow Beach',773512);
+-- INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-05-01',28433,'Eagle Peak',161980);
+-- INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-05-11',47664,'Eagle Peak',161980);
+-- INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2021-10-23',53456,'Bear River',161980);
 
 --
 -- Distrobutions
 --
 -- Released, Hatched, Tagged Distributions
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS, LifeStage) VALUES ('2020-01-02',12345,'Lake Emmitt',167680, 'Juvenile');
+INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2020-01-02',12345,'Lake Emmitt',167680);
 INSERT INTO RELEASED (Distribution_ID, Latitude, Longitude) VALUES (LAST_INSERT_ID(), 34.3425, 65.2352);
-INSERT INTO HATCHED_DISTRIBUTION (Average_length,Average_weight) VALUES (3.34, .24);
+INSERT INTO HATCHED_DISTRIBUTION (Average_length, Average_weight, life_stage) VALUES (3.34, .24, 'Juvenile');
 UPDATE RELEASED SET HID = LAST_INSERT_ID();
 INSERT INTO TAGGED_DISTRIBUTION (tag_type, percent_tagged, HID) VALUES ('anchor', 5, LAST_INSERT_ID());
 
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS, LifeStage) VALUES ('2020-02-01',23462,'Lake Emmitt',167678, 'Juvenile');
+INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2020-02-01',23462,'Lake Emmitt',167678);
 INSERT INTO RELEASED (Distribution_ID, Latitude, Longitude) VALUES (LAST_INSERT_ID(), 35.5265, 62.5432);
-INSERT INTO HATCHED_DISTRIBUTION (Average_length,Average_weight,HID) VALUES (4.74, .423, LAST_INSERT_ID());
+INSERT INTO HATCHED_DISTRIBUTION (Average_length, Average_weight, life_stage) VALUES (4.74, .423,'Juvenile');
 INSERT INTO TAGGED_DISTRIBUTION (tag_type, percent_tagged, HID) VALUES ('PIT', 3, LAST_INSERT_ID());
 
 -- Released, Hatched Distributions
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS, LifeStage) VALUES ('2020-03-10',22334,'Liberty',167680, 'Adult');
+INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2020-03-10',22334,'Liberty',167680);
 INSERT INTO RELEASED (Distribution_ID, Latitude, Longitude) VALUES (LAST_INSERT_ID(), 73.3753, 63.4986);
-INSERT INTO HATCHED_DISTRIBUTION (Average_length,Average_weight,HID) VALUES (10.43, 3.245, LAST_INSERT_ID());
+INSERT INTO HATCHED_DISTRIBUTION (Average_length, Average_weight, life_stage) VALUES (10.43, 3.245, 'Adult');
+UPDATE RELEASED SET HID = LAST_INSERT_ID();
 
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS, LifeStage) VALUES ('2020-08-21',13456,'Heinicke',167678, 'Adult');
+INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2020-08-21',13456,'Heinicke',167678);
 INSERT INTO RELEASED (Distribution_ID, Latitude, Longitude) VALUES (LAST_INSERT_ID(), 44.0752, 25.6502);
-INSERT INTO HATCHED_DISTRIBUTION (Average_length,Average_weight,HID) VALUES (5.13, 1.005, LAST_INSERT_ID());
+INSERT INTO HATCHED_DISTRIBUTION (Average_length,Average_weight, life_stage) VALUES (5.13, 1.005, 'Adult');
 
 -- Released Distributions
-INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS, LifeStage) VALUES ('2020-09-13',17245,'Heinicke',167680, 'Egg');
+INSERT INTO DISTRIBUTION (Date, Count, Fname, S_ITIS) VALUES ('2020-09-13',17245,'Heinicke',167680);
 INSERT INTO RELEASED (Distribution_ID, Latitude, Longitude) VALUES (LAST_INSERT_ID(), 43.5032, 26.0131);
 
 -- Transfer Distribution
